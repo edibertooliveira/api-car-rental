a4b46c1985fc236c29e4a3fdc529d9a83e01f8b6
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-27 12:35:27 -0300

    fix: added config jest

--- jest.config.ts
+++ jest.config.ts
@@ -41,7 +41,14 @@ export default {
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
@@ -174,10 +181,9 @@ export default {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
@@ -186,7 +192,9 @@ export default {
   // verbose: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
+  watchPathIgnorePatterns: [
+    "/node_modules/",
+  ],
 
   // Whether to use watchman for file crawling
   // watchman: true,
---
+++ src/__tests__/unit/carService.test.ts
@@ -0,0 +1,17 @@
+import {
+  describe,
+  test,
+  // expect,
+} from '@jest/globals'
+
+describe('cars', ()=> {
+
+  describe('CraeteCarService', ()=> {
+    test.todo('test one')
+  })
+
+  describe('ListCarsService', ()=> {
+    test.todo('test one')
+  })
+
+})
