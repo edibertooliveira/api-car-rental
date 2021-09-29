4d4854519eaac3038780a95b00552b513506822c
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-29 16:47:17 -0300

    feat: added routers car

---
+++ src/modules/cars/controllers/index.ts
@@ -0,0 +1,4 @@
+import CarProfileController from './CarProfileController';
+import CarsController from './CarsController';
+
+export { CarsController, CarProfileController };
--- src/shared/app.ts
+++ src/shared/app.ts
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(router);
+app.use('/api/v1', router);
 
 app.use(
   '/docs',
